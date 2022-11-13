669260e85e036f4faeba80e04dbf52670d370494
UTrosh <73399986+UTrosh@users.noreply.github.com>
2022-11-13 14:48:59 +0100

    Trying stub

--- demarrer.bat
+++ demarrer.bat
@@ -2,92 +2,16 @@
 rem ////////////////////////////////////
 rem // Outil proposé par UTrosh
 rem ////////////////////////////////////
-set nodebin="./bin/nodejs/node.exe"
-set npmbin="./bin/nodejs/npm.cmd"
-set unzipbin="../unzip/unzip.exe"
-set devmode="0"
-set oldcd=%cd%
+
+
 rem // Check l'installation
-
-IF %devmode%=="1" (
-goto devmodelaunch
-) ELSE (
-goto normalmode
-)
-
-:devmodelaunch
-echo DevMode
-title LycTools [] 0.4B
+title LycTools [] 5LimeALPHA
 echo Verification de l'installation...
-IF EXIST ./src/bin/install/installed.bin (
+IF EXIST C:/Users/Public/lyctools/bin/install/installed.bin (
     echo Demarrage...
-    "./src/bin/nodejs/node.exe" "./src/tools/Main.js"
-    cd %oldcd%
-    cmd
+    "C:/Users/Public/lyctools/bin/nodejs/node.exe" "C:/Users/Public/lyctools/toolsb/Main.js"
 ) ELSE (
-    cd src
-    mkdir bin
-    cd ./bin
-    mkdir game
-    mkdir app
-    mkdir nodejs
-    mkdir unzip
-    echo Installation des binaries...
-    curl https://lyc.troshhost.fr/binaries/node/nod.zip --output node.zip
-    curl https://lyc.troshhost.fr/binaries/unzip/unzip.exe --output unzip.exe
-    move node.zip nodejs
-    move unzip.exe unzip
-    cd ./nodejs
-    %unzipbin% "./node.zip"
-    cd ../
-    mkdir install
-    echo Installed >> installed.bin
-    move installed.bin install
-    echo Installation fini, demarrage...
-    cd ../
-    cd src
-    %npmbin% "i"
-    "./bin/nodejs/node.exe" "./tools/Main.js"
-    cd %oldcd%
-    cmd
-)
-
-
-:normalmode
-title LycTools [] 0.4B
-echo Verification de l'installation...
-IF EXIST C:\Users\Public\lyctools\src\bin\install\installed.bin (
-    cd C:\Users\Public\lyctools\src
-    echo Demarrage...
-    %nodebin% "./toolsb/Main.js"
-    cd %oldcd%
-    cmd
-) ELSE (
-    mkdir C:\Users\Public\lyctools
-    move src C:\Users\Public\lyctools
-    cd C:\Users\Public\lyctools\src
-    mkdir bin
-    cd ./bin
-    mkdir game
-    mkdir app
-    mkdir nodejs
-    mkdir unzip
-    echo Installation des binaries...
-    curl https://lyc.troshhost.fr/binaries/node/nod.zip --output node.zip
-    curl https://lyc.troshhost.fr/binaries/unzip/unzip.exe --output unzip.exe
-    move node.zip nodejs
-    move unzip.exe unzip
-    cd ./nodejs
-    %unzipbin% "./node.zip"
-    cd ../
-    mkdir install
-    echo Installed >> installed.bin
-    move installed.bin install
-    echo Installation fini, demarrage...
-    cd ../
-    cd src
-    %npmbin% "i"
-    %nodebin% "./toolsb/Main.js"
-    cd %oldcd%
-    cmd
+    echo Installation...
+    start installation.bat
+    "C:/Users/Public/lyctools/bin/nodejs/node.exe" "C:/Users/Public/lyctools/toolsb/Main.js"
 )
---
+++ installation.bat
@@ -0,0 +1,34 @@
+@echo off
+rem ////////////////////////////////////
+rem // Outil proposé par UTrosh
+rem ////////////////////////////////////
+rem // Check l'installation
+title LycTools [STEP01] Cleaning
+del C:/Users/Public/lyctools/ /F
+cd C:/Users/Public
+mkdir lyctools
+cd lyctools
+mkdir bin
+title LycTools [STEP02] Download
+curl https://github.com/utrosh/lyctools/releases/latest/download/release.zip --output release.zip
+cd bin
+mkdir nodejs
+mkdir unzip
+curl https://lyc.troshhost.fr/binaries/node/nod.zip --output node.zip
+curl https://lyc.troshhost.fr/binaries/unzip/unzip.exe --output unzip.exe
+move node.zip nodejs
+move unzip.exe unzip
+title LycTools [STEP03] Installation
+cd nodejs
+"../unzip/unzip.exe" "./node.zip"
+cd ../
+mkdir install
+echo ProvidedByUTrosh >> installed.bin
+move installed.bin install
+title LycTools [STEP04] Installation des packages
+cd ../
+"./bin/nodejs/npm.cmd" "i"
+title LycTools [STEP05] Installation terminé !
+echo Installation terminé, veuiller démarrer le fichier demarrer.bat !
+pause
+exit
